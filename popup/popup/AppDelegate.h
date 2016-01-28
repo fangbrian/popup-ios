74ccd8f1bc5a2134e613a85ca7f8cda127ccc66c
Brian Fang <fangbrian@gmail.com>
2016-01-27 19:22:08 -0800

    Initial Commit

---
+++ popup/popup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  popup
+//
+//  Created by Brian Fang  on 1/27/16.
+//  Copyright © 2016 The Vampire Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
